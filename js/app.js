d992d32ba9f82fa1197e7df0d440dd39e33d5792
decioneto <decio.neeto@gmail.com>
2020-11-17 00:46:20 -0300

    Filter menu slide animation

--- js/app.js
+++ js/app.js
@@ -1,14 +1,30 @@
-var dropFilter = document.getElementsByClassName("section-header")
-var i;
+let dropFilter = document.getElementsByClassName("section-header")
+let i;
 
 for (i = 0; i < dropFilter.length; i++) {
     dropFilter[i].addEventListener('click', function() {
         this.classList.toggle("active");
-        var panel = this.nextElementSibling;
+        let panel = this.nextElementSibling;
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
         } else {
             panel.style.maxHeight = panel.scrollHeight + "px";
         }
     });
+}
+
+function closeFilter() {
+    let asideFilter = document.getElementById("aside-popper")
+    let btnCloseFilter = document.getElementById("close-filter")
+    let filterCol = document.getElementsByClassName("filter-col")
+       
+    asideFilter.classList.toggle("active");
+
+    if(asideFilter.classList.contains("active")) {
+        btnCloseFilter.style.display = "block";
+        // filterCol.classList.add("col-lg-2");
+    } else {
+        btnCloseFilter.style.display = "none";
+        // filterCol.classList.add("col-lg-1");
+    }
 }
